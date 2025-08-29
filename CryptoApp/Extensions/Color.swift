de4ce440d6232f2130593788766bbcbcdff3852e
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-08-29 08:29:18 -0300

    feat: setup colors

---
+++ CryptoApp/Extensions/Color.swift
@@ -0,0 +1,22 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Luis Filipe Pedroso on 29/08/25.
+//
+import SwiftUI
+
+extension Color {
+   
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
