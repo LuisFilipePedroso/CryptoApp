234e9c16387d2cfe08aa4f3f5fb8e4822665d040
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-09-03 19:56:02 -0300

    feat: create circle button component

---
+++ CryptoApp/Core/Components/CircleButton/CircleButtonView.swift
@@ -0,0 +1,41 @@
+//
+//  CircleButtonView.swift
+//  CryptoApp
+//
+//  Created by Luis Filipe Pedroso on 29/08/25.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    
+    let icon: String
+    var width: CGFloat = 50
+    var height: CGFloat = 50
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 16, weight: .medium))
+            .foregroundStyle(Color.theme.accent)
+            .frame(width: width, height: height)
+            .background {
+                Circle()
+                    .foregroundStyle(Color.theme.background)
+            }
+            .shadow(
+                color: Color.theme.accent.opacity(0.25),
+                radius: 10, x: 0, y: 0
+            )
+            .padding()
+    }
+}
+
+#Preview {
+    Group {
+        CircleButtonView(icon: "info")
+            .padding()
+        
+        CircleButtonView(icon: "plus")
+            .colorScheme(.dark)
+    }
+}
