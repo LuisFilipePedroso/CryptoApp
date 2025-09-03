7357e54e82ccd84935a526356b7b81ca8701067e
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-09-03 19:56:31 -0300

    feat: create header

--- CryptoApp/Core/Home/Views/HomeView.swift
+++ CryptoApp/Core/Home/Views/HomeView.swift
@@ -8,19 +8,57 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    @Namespace private var namespace
+    
     var body: some View {
         ZStack {
             Color.theme.background
                 .ignoresSafeArea()
             
             VStack {
-                Text("Header")
-                Spacer(minLength: 10)
+                HStack {
+                    if showPortfolio {
+                        CircleButtonView(icon: "plus", width: 40, height: 40)
+                        .transaction { $0.disablesAnimations = true }
+                            .matchedGeometryEffect(id: "portfolio", in: namespace)
+                            
+                    } else {
+                        CircleButtonView(icon: "info", width: 40, height: 40)
+                        .transaction { $0.disablesAnimations = true }
+                            .matchedGeometryEffect(id: "portfolio", in: namespace)
+                            
+                    }
+                    
+                    Spacer()
+                    
+                    Text(showPortfolio ? "Portfolio" : "Live prices")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundStyle(Color.theme.accent)
+                        .transaction { $0.disablesAnimations = true }
+                    
+                    Spacer()
+                    
+                    CircleButtonView(icon: "chevron.right", width: 40, height: 40)
+                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                showPortfolio.toggle()
+                            }
+                        }
+                }
+                .padding(.horizontal)
+                Spacer(minLength: 0)
             }
         }
     }
 }
 
 #Preview {
-    HomeView()
+    NavigationView {
+        HomeView()
+            .toolbar(.hidden)
+    }
 }
