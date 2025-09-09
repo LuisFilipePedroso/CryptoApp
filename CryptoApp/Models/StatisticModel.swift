2cd21c4c7d0174ae595c5bdeaaffc6ad825f5735
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-09-09 20:14:15 -0300

    feat: create statistic model

---
+++ CryptoApp/Models/StatisticModel.swift
@@ -0,0 +1,21 @@
+//
+//  StatisticModel.swift
+//  CryptoApp
+//
+//  Created by Luis Filipe Pedroso on 09/09/25.
+//
+
+import Foundation
+
+struct StatisticModel: Identifiable {
+    let id = UUID().uuidString
+    let title: String
+    let value: String
+    let percentageChange: Double?
+    
+    init(title: String, value: String, percentageChange: Double? = nil) {
+        self.title = title
+        self.value = value
+        self.percentageChange = percentageChange
+    }
+}
