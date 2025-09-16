1ca1eb959a4b4264d346dc97cb07bb28847c81e6
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-09-16 08:27:06 -0300

    feat: init detail view

---
+++ CryptoApp/Core/Detail/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  CryptoApp
+//
+//  Created by Luis Filipe Pedroso on 16/09/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
