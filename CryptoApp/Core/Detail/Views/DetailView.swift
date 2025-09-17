890c4cdf2ce977631a45fd0189bb5906d4772e8c
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-09-16 21:19:37 -0300

    feat: create detail section

--- CryptoApp/Core/Detail/Views/DetailView.swift
+++ CryptoApp/Core/Detail/Views/DetailView.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct DetailView: View {
+    
+    @StateObject var vm: DetailViewModel
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    private let spacing: CGFloat = 32
+
+    init(coin: CoinModel) {
+        self._vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 20) {
+                    Spacer(minLength: 150)
+                    
+                    DetailSection(title: "Overview", iterateOver: vm.overviewStatistics)
+                    DetailSection(title: "Additional Details", iterateOver: vm.additionalStatistics)
+                }
+                .padding()
+            }
+            .navigationTitle(vm.coin.name)
+            .navigationBarTitleDisplayMode(.large)
+        }
     }
 }
 
 #Preview {
-    DetailView()
+    DetailView(coin: CoinModel.preview)
+}
+
+extension DetailView {
+    
+    @ViewBuilder
+    private func DetailSection(title: String, iterateOver: [StatisticModel]) -> some View {
+        Text(title)
+            .font(.title)
+            .bold()
+            .foregroundStyle(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+        
+        Divider()
+        
+        LazyVGrid(
+            columns: columns,
+            alignment: .leading,
+            spacing: spacing,
+            content: {
+                ForEach(iterateOver) { data in
+                    StatisticView(stat: StatisticModel(title: data.title, value: data.value))
+                }
+            }
+        )
+    }
 }
