6a697a4456336a3f3408426506c5c187a6e075be
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2025-09-19 08:40:57 -0300

    feat: starting the settings view

---
+++ CryptoApp/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  CryptoApp
+//
+//  Created by Luis Filipe Pedroso on 19/09/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
